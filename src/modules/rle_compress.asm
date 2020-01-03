6ff80eb278a8eb27edc8fee0c20cd297b48bf33c
Filip van Vooren <filip.van-vooren@oratronik.de>
2020-01-03 22:53:24 +0100

    Many bugs fixed

--- src/modules/rle_compress.asm
+++ src/modules/rle_compress.asm
@@ -52,7 +52,7 @@
 *             
 *  (3) End of string reached:
 *      (3.1) Check if pending repeated character
-*      (3.2) If yes, flush to output buffer (=RLE encode) and exit
+*      (3.2) If yes, flush pending to output buffer (=RLE encode)
 *      (3.3) Check if pending non-repeated character
 *      (3.4) If yes, set encoding byte before first data byte
 *
@@ -65,14 +65,15 @@ cpu2rle:
         mov   *r11+,tmp0            ; ROM/RAM source address
         mov   *r11+,tmp1            ; RAM target address
         mov   *r11+,tmp2            ; Length of data to encode
-xcpu2rle:        
-        mov   r11,@waux3            ; Save return address        
+xcpu2rle:
+        dect  stack        
+        mov   r11,*stack            ; Save return address        
 *--------------------------------------------------------------
 *   Initialisation
 *--------------------------------------------------------------
 cup2rle.init:
         clr   tmp3                  ; Character buffer (2 chars)
-        clr   tmp4                  ; Repetition counter
+        clr   tmp4                  ; Repeat counter
         clr   @waux1                ; Length of RLE string        
         clr   @waux2                ; Address of encoding byte
 *--------------------------------------------------------------
@@ -81,18 +82,26 @@ cup2rle.init:
 cpu2rle.scan:
         srl   tmp3,8                ; Save old character in LSB
         movb  *tmp0+,tmp3           ; Move current character to MSB
-        cb    *tmp0,tmp3            ; Compare lookahead with current
+        cb    *tmp0,tmp3            ; Compare 1st lookahead with current.
+        jne   cpu2rle.scan.nodup    ; No duplicate, move along
+
+        cb    @1(tmp0),tmp3         ; Compare 2nd lookahead with current.
+        jne   cpu2rle.scan.nodup    ; Only 1 repeated char, compression is 
+                                    ; not worth it, so move along
+
         jeq   cpu2rle.scan.dup      ; Duplicate found
 *--------------------------------------------------------------
 *   (1.2) No duplicate
 *--------------------------------------------------------------
 cpu2rle.scan.nodup:
         ;------------------------------------------------------
-        ; (1.2.1) First check if any pending duplicates
+        ; (1.2.1) First flush any pending duplicates
         ;------------------------------------------------------
-        mov   tmp4,tmp4
-        jne   cpu2rle.scan.flush_dup 
-                                    ; Yes, flush pending duplicates
+        mov   tmp4,tmp4             ; Repeat counter = 0 ?
+        jeq   cpu2rle.scan.nodup.rest
+
+        bl    @cpu2rle.flush.duplicates
+                                    ; Flush pending duplicates
         ;------------------------------------------------------
         ; (1.2.3) Track address of encoding byte
         ;------------------------------------------------------
@@ -114,44 +123,20 @@ cpu2rle.scan.nodup.rest:
 *--------------------------------------------------------------
 cpu2rle.scan.dup:
         ;------------------------------------------------------
-        ; (1.3.1) First check if any pending non-duplicates
+        ; (1.3.1) First flush any pending non-duplicates
         ;------------------------------------------------------
-        mov   @waux2,@waux2
-        jne   cpu2rle.scan.flush_nodup 
-                                    ; Set encoding byte before 1st data byte
+        mov   @waux2,@waux2         ; Pending encoding byte address present?
+        jeq   cpu2rle.scan.dup.rest
+
+        bl    @cpu2rle.flush.encoding_byte
+                                    ; Set encoding byte before 
+                                    ; 1st data byte of unique string
         ;------------------------------------------------------
         ; (1.3.3) Now process duplicate character
         ;------------------------------------------------------
 cpu2rle.scan.dup.rest:
-        inc   tmp4                  ; Increase repetition counter
+        inc   tmp4                  ; Increase repeat counter
         jmp   cpu2rle.scan.next     ; Scan next character
-
-
-*--------------------------------------------------------------
-*   (1.2.2) Flush duplicate to output buffer (=RLE encode)
-*--------------------------------------------------------------
-cpu2rle.scan.flush_dup:
-        movb  tmp3,tmp4             ; Move character to MSB 
-        swpb  tmp4                  ; Move counter to MSB, character to LSB
-        ori   tmp4,8000             ; Set high bit in MSB
-        mov   tmp4,*tmp1+           ; RLE word to output buffer        
-        clr   tmp4                  ; Reset repetition counter
-        inct  @waux1                ; RLE string length += 2
-        jmp   cpu2rle.scan.nodup.rest
-*--------------------------------------------------------------
-*   (1.3.2) Set encoding byte before first data byte
-*--------------------------------------------------------------
-cpu2rle.scan.flush_nodup:
-        mov   tmp1,tmp4             ; \ Calculate length of non-repeated  
-        s     @waux2,tmp4           ; | characters. Short on registers
-        dec   tmp4                  ; / so abusing tmp4
-
-        sla   tmp4,8                ; \
-        movb  tmp4,@waux2           ; / Set encoding byte 
-
-        clr   @waux2                ; Reset address of encoding byte   
-        clr   tmp4                  ; Clear before using again
-        jmp   cpu2rle.scan.dup.rest
 
 *--------------------------------------------------------------
 *   (2) Next character
@@ -159,44 +144,84 @@ cpu2rle.scan.flush_nodup:
 cpu2rle.scan.next:
         dec   tmp2
         jgt   cpu2rle.scan          ; (2.1) Next compare
+
 *--------------------------------------------------------------
 *   (3) End of string reached
 *--------------------------------------------------------------
-cpu2rle.eos.pending_dup:
         ;------------------------------------------------------
-        ; (3.1) End of string. Pending duplicates left?
+        ; (3.1) Flush any pending duplicates
         ;------------------------------------------------------       
-        mov   tmp4,tmp4             ; Duplicates left?
-        jeq   cpu2rle.eos.pending_nodup
-                                    ; No, check no-duplicates
+cpu2rle.eos.check1:
+        mov   tmp4,tmp4             ; Repeat counter = 0 ?
+        jeq   cpu2rle.eos.check2
+
+        bl    @cpu2rle.flush.duplicates
+                                    ; (3.2) Flush pending 
+        jmp   cpu2rle.$$            ;       duplicates and exit
         ;------------------------------------------------------
-        ; (3.2) Yes, flush pending duplicates to output buffer
+        ; (3.3) Flush any pending encoding byte
         ;------------------------------------------------------
-cpu2rle.eos.flush_dup:
-        movb  tmp3,tmp4             ; Move character to MSB 
-        swpb  tmp4                  ; Move counter to MSB, character to LSB
-        ori   tmp4,8000             ; Set high bit in MSB
-        mov   tmp4,*tmp1+           ; RLE encoded to output buffer
-        inct  @waux1                ; RLE string length += 2        
-        jmp   cpu2rle.$$            ; Exit
-        ;------------------------------------------------------
-        ; (3.3) End of string. Pending non-duplicates left?
-        ;------------------------------------------------------
-cpu2rle.eos.pending_nodup:
+cpu2rle.eos.check2:        
         mov   @waux2,@waux2
         jeq   cpu2rle.$$            ; No, so exit
-        ;------------------------------------------------------
-        ; (3.4) Yes, Set encoding byte before first data byte
-        ;------------------------------------------------------
-        mov   tmp1,tmp4             ; \ Calculate length of non-repeated  
-        s     @waux2,tmp4           ; | characters. Short on registers
-        dec   tmp4                  ; / so abusing tmp4
 
-        sla   tmp4,8                ; \
-        movb  tmp4,@waux2           ; / Set encoding byte         
+        bl    @cpu2rle.flush.encoding_byte
+                                    ; (3.4) Set encoding byte before 
+                                    ; 1st data byte of unique string
+
+
 *--------------------------------------------------------------
 *   (4) Exit
 *--------------------------------------------------------------
 cpu2rle.$$:
-        mov   @waux3,r11            ; Get return address
+        b     @poprt                ; Return
+
+
+
+
+*****************************************************************
+* Helper routines called internally
+*****************************************************************
+
+*--------------------------------------------------------------
+* Flush duplicate to output buffer (=RLE encode)
+*--------------------------------------------------------------
+cpu2rle.flush.duplicates:
+        swpb  tmp3                  ; Need the "old" character in buffer 
+
+        movb  tmp3,tmp4             ; Move character to MSB 
+        swpb  tmp4                  ; Move counter to MSB, character to LSB
+
+        ori   tmp4,>8000            ; Set high bit in MSB
+        movb  tmp4,*tmp1+           ; \ Write encoding byte and data byte
+        swpb  tmp4                  ; | Could be on uneven address so using
+        movb  tmp4,*tmp1+           ; / movb instruction instead of mov
+        inct  @waux1                ; RLE string length += 2                
+        ;------------------------------------------------------
+        ; Exit
+        ;------------------------------------------------------
+cpu2rle.flush.duplicates.exit:
+        swpb  tmp3                  ; Back to "current" character in buffer
+        clr   tmp4                  ; Clear repeat count
+        b     *r11                  ; Return
+
+
+*--------------------------------------------------------------
+*   (1.3.2) Set encoding byte before first data byte
+*--------------------------------------------------------------
+cpu2rle.flush.encoding_byte:
+        dect  stack                 ; Short on registers, save tmp3 on stack
+        mov   tmp3,*stack           ; No need to save tmp4, but reset before exit
+
+        mov   tmp1,tmp3             ; \ Calculate length of non-repeated  
+        s     @waux2,tmp3           ; | characters
+        dec   tmp3                  ; /
+
+        sla   tmp3,8                ; Left align to MSB
+        mov   @waux2,tmp4           ; Destination address of encoding byte
+        movb  tmp3,*tmp4            ; Set encoding byte
+
+        mov   *stack+,tmp3          ; Pop tmp3 from stack
+        clr   @waux2                ; Reset address of encoding byte   
+        clr   tmp4                  ; Clear before exit
         b     *r11                  ; Return
